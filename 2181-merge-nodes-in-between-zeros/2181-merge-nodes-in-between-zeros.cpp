3eb19301ce5ddc7df9c8bce971c00b0dc4bb748b
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-10-29 18:36:02 +0200

    Time: 2093 ms (5.04%), Space: 274.5 MB (30.38%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* h = nullptr;
+        ListNode* ans = nullptr;
+        auto sum = 0;
+        head=head->next;
+        while(head) {
+            if(head->val == 0) {
+                cout << "dupa ";
+                auto node = new ListNode(sum);
+                if(!h) {
+                    h = node;
+                    ans = h;
+                }
+                else {
+                    h->next = node;
+                    h = h->next;
+                }
+                sum = 0;
+            }
+            sum += head->val;
+            head = head->next;
+        }
+        cout << '\n';
+        return ans;
+    }
+};
